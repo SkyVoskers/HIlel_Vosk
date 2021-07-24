dcf06c19a2d235878bc497a8ea6c10a9499744a9
Voskers <voskolup11@gmail.com>
2021-07-24 18:40:00 +0300

    \ added

--- Homework_2/Task_Escape_Symbols.py
+++ Homework_2/Task_Escape_Symbols.py
@@ -1,6 +1,8 @@
 print(
-    "ESCAPE Symbols prompt:\n" + r"\a - Bell(alert)" + "\n" + r"\b - Backspace" + "\n" + r"\n - New Line" + "\n" + r"\t - Horizontal Tab" + "\n" + r"\\ - Backslash() \ " + "\n" +
-    r'" - Double Quotation Mark(")' + "\n" + r"' - Single Quotation Mark(')")
+    "ESCAPE Symbols prompt:\n" + r"\a - Bell(alert)" + "\n"
+    + r"\b - Backspace" + "\n" + r"\n - New Line" + "\n"
+    + r"\t - Horizontal Tab" + "\n" + r"\\ - Backslash() \ " + "\n"
+    + r'" - Double Quotation Mark(")' + "\n" + r"' - Single Quotation Mark(')")
 # Escape sequences
 # \Name    -  Bell (alert)
 # \b    -  Backspace

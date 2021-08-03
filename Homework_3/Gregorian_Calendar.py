7156eb83a8e3e2ea01f609ee70a3547b86ad4c96
Voskers <voskolup11@gmail.com>
2021-08-03 11:33:12 +0300

    Homework_Lesson_3

--- Homework_3/Gregorian_Calendar.py
+++ Homework_3/Gregorian_Calendar.py
@@ -1,7 +1,7 @@
 while(True):
     print("Input year to check whether it LEAP or NOT in Gregorian_Calendar : ")
     year=int(input())
-    if (year % 400== 0 or year % 4 ==0):
+    if (year % 400== 0 and year  or year % 4 ==0 and year %100!=0):
         print("Input year", year, "is leap")
     else:
         print("Input year", year, "is not leap")
